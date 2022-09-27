027f3f842b2372ec4e3938615a6193af9f49654f
PikPakShare <113594376+PikPakShare@users.noreply.github.com>
2022-09-28 03:37:20 +0800

    删除测试功能

--- src/router/index.ts
+++ src/router/index.ts
@@ -49,11 +49,6 @@ const routes: RouteRecordRaw[] = [
         name: 'invited',
         component: () => import('../views/invited.vue')
       },
-      // {
-      //   path: 'share',
-      //   name: 'share',
-      //   component: () => import('../views/share.vue')
-      // }
     ]
   },
   {
@@ -80,11 +75,6 @@ const routes: RouteRecordRaw[] = [
     path: '/register',
     name: 'register',
     component: () => import('../views/register.vue'),
-  },
-  {
-    path: '/testtest',
-    name: 'testtest',
-    component: () => import('../views/testtest.vue'),
   },
 ]
 
